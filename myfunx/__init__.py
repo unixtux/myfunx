e83d54f0b5834d8eccd8a73d2e041747f78ce0aa
gekone1 <ivangasparott@gmail.com>
2024-04-01 14:37:53 +0200

    0.5.1, _logging.py -> logging.py

--- myfunx/__init__.py
+++ myfunx/__init__.py
@@ -2,10 +2,10 @@
 
 __all__ = ('Client', 'JsonManager',)
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 VERSION = __version__
 
-from aiotgm._logging import get_logger
+from aiotgm.logging import get_logger
 logger = get_logger('myfunx ' + VERSION)
 del get_logger
 
